c71e82e556b9872f927aaaab3494d184385bf971
GABRIELAUGUSTOKAPPENBERG <kappenberg4@gmail.com>
2025-03-20 21:52:02 -0300

    Adicionado o ReadKey

--- Algoritmo.1/Algoritmo.1/Program.cs
+++ Algoritmo.1/Algoritmo.1/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//Esperar o usuario pressionar uma tecla
+Console.ReadKey();
+//Metodo sempre é seguido por ()
